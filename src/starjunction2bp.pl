a22b2b317b276fe9031d176477b1bc86e063ee90
Ruping Sun % Califano <rs3412@v28a-hb5.titan>
2014-10-01 02:28:49 +0000

    star

---
+++ src/starjunction2bp.pl
@@ -0,0 +1,37 @@
+use strict;
+
+my $breakpoints = shift;
+my $starjunction = shift;
+
+open IN, "$breakpoints";
+my %breakpoints;
+while ( <IN> ){
+  chomp;
+  my @cols = split /\t/;
+  $breakpoints{$cols[2]} = '';
+}
+close IN;
+
+open IN, "$starjunction";
+while ( <IN> ) {
+  chomp;
+  my ($chr1, $pos1, $strand1, $chr2, $pos2, $strand2, $junctionType, $repLeft, $repRight, $read, $loc1, $cigar1, $loc2, $cigar2) = split /\t/;
+  my $pow = 'p';
+  if (($chr1 eq $chr2) and (abs($pos1-$pos2) < 230000)) {
+     $pow = 'w';
+  }
+  next if (exists($breakpoints{$read}));
+  while ( $cigar1 =~ /(\d+)S/g ) {
+    if ( $1 > 20 ){
+       printf("%s\n", join("\t", $chr1, $pos1, $read, $pow, 'S'));
+       last;
+    }
+  }
+  while ( $cigar2 =~ /(\d+)S/g ) {
+    if ( $1 > 20 ){
+       printf("%s\n", join("\t", $chr2, $pos2, $read, $pow, 'S'));
+       last;
+     }
+  }
+}
+close IN;
