2b02ac160eb0dfc4846eed6f36920330ed88b495
Ruping Sun % Califano <rs3412@v27a-hb5.titan>
2014-10-02 20:53:21 +0000

    star breakpoint

--- bin/starjunction2bp.pl
+++ bin/starjunction2bp.pl
@@ -1,6 +1,7 @@
 use strict;
 
 my $breakpoints = shift;
+#my $lastbpid = shift;
 my $starjunction = shift;
 
 open IN, "$breakpoints";
@@ -16,22 +17,22 @@ open IN, "$starjunction";
 while ( <IN> ) {
   chomp;
   my ($chr1, $pos1, $strand1, $chr2, $pos2, $strand2, $junctionType, $repLeft, $repRight, $read, $loc1, $cigar1, $loc2, $cigar2) = split /\t/;
-  my $pow = 'p';
+  my $pow = 'w';
   if (($chr1 eq $chr2) and (abs($pos1-$pos2) < 230000)) {
-     $pow = 'w';
+     $pow = 'p';
   }
   next if (exists($breakpoints{$read}));
   while ( $cigar1 =~ /(\d+)S/g ) {
-    if ( $1 > 20 ){
+    if ( $1 > 20 ) {
        printf("%s\n", join("\t", $chr1, $pos1, $read, $pow, 'S'));
        last;
     }
   }
   while ( $cigar2 =~ /(\d+)S/g ) {
-    if ( $1 > 20 ){
+    if ( $1 > 20 ) {
        printf("%s\n", join("\t", $chr2, $pos2, $read, $pow, 'S'));
        last;
-     }
+    }
   }
 }
 close IN;
